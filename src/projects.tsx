9833508bef6e3f8c4ad86655c2347d9a44f36c49
Firhan Ramadhan <rfirhan99@gmail.com>
2024-03-24 21:08:40 +0700

    feat: create projects command

---
+++ src/projects.tsx
@@ -0,0 +1,9 @@
+import { Icon, List } from "@raycast/api";
+
+export default function Projects() {
+  return (
+    <List>
+      <List.Item title="Project" icon={Icon.Layers} />
+    </List>
+  );
+}
