16c02d688ba620501a97523f6ff52e3329ed62b9
Firhan Ramadhan <rfirhan99@gmail.com>
2024-03-25 04:05:58 +0700

    feat: add 403

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -2,7 +2,6 @@ import { Action, ActionPanel, Detail, openCommandPreferences } from "@raycast/ap
 import { ReactNode } from "react";
 
 export default function Error({ errCode, revalidate }: { errCode: string; revalidate?: () => void }) {
-  console.log(errCode);
   const getError: () => {
     message: string;
     actions: ReactNode;
@@ -16,6 +15,11 @@ export default function Error({ errCode, revalidate }: { errCode: string; revali
               <Action title="Open Extension Preferences" onAction={openCommandPreferences} />
             </ActionPanel>
           ),
+        };
+      case "403":
+        return {
+          message: "Forbidden. You don't have permission to access this resource",
+          actions: null,
         };
       default:
         return {
