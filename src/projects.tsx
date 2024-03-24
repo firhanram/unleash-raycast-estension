84d3d529ff3dd90ee9beea46625b94cb0601153d
Firhan Ramadhan <rfirhan99@gmail.com>
2024-03-24 21:28:57 +0700

    feat: add list projects

--- src/projects.tsx
+++ src/projects.tsx
@@ -1,9 +1,28 @@
 import { Icon, List } from "@raycast/api";
+import { useGetAllProjects } from "./hooks/useGetAllProjects";
 
 export default function Projects() {
+  const { data, isLoading } = useGetAllProjects();
+
   return (
-    <List>
-      <List.Item title="Project" icon={Icon.Layers} />
+    <List isLoading={isLoading} searchBarPlaceholder="Search Projects...">
+      {data?.map((project) => (
+        <List.Item
+          title={project.name}
+          key={project.name}
+          icon={Icon.Layers}
+          subtitle={`#${project.id}`}
+          accessories={[
+            {
+              tag: `${project.featureCount} Toggles`,
+              tooltip: "Total number of toggles in this project",
+            },
+            {
+              icon: Icon.ChevronRight,
+            },
+          ]}
+        />
+      ))}
     </List>
   );
 }
