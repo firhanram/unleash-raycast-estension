93203db2d7a006d7215c69614f4f08461999f0d4
Firhan Ramadhan <rfirhan99@gmail.com>
2024-03-25 12:53:38 +0700

    feat: add field validation

--- src/views/CreateFeature.tsx
+++ src/views/CreateFeature.tsx
@@ -1,7 +1,11 @@
 import { Action, ActionPanel, Form } from "@raycast/api";
 import { TFeature } from "../types";
+import { useState } from "react";
 
 export default function CreateFeature({ revalidate }: { revalidate: () => Promise<TFeature[]> }) {
+  const [featureName, setFeatureName] = useState("");
+  const [featureNameError, setFeatureNameError] = useState("");
+
   return (
     <Form
       actions={
@@ -16,12 +20,39 @@ export default function CreateFeature({ revalidate }: { revalidate: () => Promis
         </ActionPanel>
       }
     >
-      <Form.TextField id="name" title="Name" info="Unique feature name" placeholder="Name" />
+      <Form.TextField
+        id="name"
+        title="Name"
+        info="Unique feature name"
+        placeholder="Name"
+        value={featureName}
+        onChange={(val) => {
+          const name = val.trim();
+          setFeatureName(name);
+
+          if (name.length > 0) {
+            setFeatureNameError("");
+          }
+        }}
+        error={featureNameError}
+        onBlur={(ev) => {
+          const value = ev.target.value?.trim();
+
+          if (value?.length === 0) {
+            setFeatureNameError("Required");
+          }
+        }}
+      />
       <Form.Dropdown id="type" title="Toggle Type" placeholder="Toggle Type" defaultValue="boolean">
         <Form.Dropdown.Item value="boolean" title="Boolean" />
       </Form.Dropdown>
       <Form.TextArea id="description" title="Description" placeholder="Description" info="Optional" />
-      <Form.Checkbox id="impressionData" label="Impression Data" defaultValue={false} />
+      <Form.Checkbox
+        id="impressionData"
+        label="Impression Data"
+        defaultValue={false}
+        info="true if the impression data collection is enabled for the feature, otherwise false."
+      />
     </Form>
   );
 }
